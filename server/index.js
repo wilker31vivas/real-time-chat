693bde89ae8bf4aa3bba0ea9f04d99900b3d020a
wilker <wilker.vivas.31@gmail.com>
2026-05-02 07:20:25 -0400

    add script on package.json

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import { createServer } from "node:http";
 
 dotenv.config();
 
-const port = process.env.port ?? 3000;
+const port = process.env.PORT ?? 3000;
 
 const app = express();
 const server = createServer(app);
